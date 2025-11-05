db305c72fb1aa935cd846e35e0a637fb226bfb91
Abijith G Kaimal <abijithgkaimal@Abijiths-MacBook-Air.local>
2025-11-05 19:48:21 +0530

    added index.js in backend

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+// import json-server
+const JSONserver = require('json-server');
+
+// create server for running json file
+const TableTrackerServer = JSONserver.create();
+
+// create middleware
+const middleware = JSONserver.defaults();
+
+// import db.json file
+const router = JSONserver.router('db.json');
+
+// define port to run the server
+const PORT = process.env.PORT || 5001;
+
+// use middleware
+TableTrackerServer.use(middleware);
+
+// use router
+TableTrackerServer.use(router);
+
+// tell server to listen client request
+TableTrackerServer.listen(PORT, () => {
+  console.log(`Table Tracker JSON Server started at PORT number ${PORT}`);
+});
